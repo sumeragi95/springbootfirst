56fcbdc03bc61fde06328ed02edf1bb44324dbed
sumeragi95 <57343325+sumeragi95@users.noreply.github.com>
2019-11-04 14:44:54 +0700

    Update MainController.java

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -55,3 +55,52 @@ public class MainController {
 		model.addAttribute("identity", identity);
 		return "editIdentity";
 	}
+@RequestMapping(value="/edit", method = RequestMethod.POST)
+	public String saveEdit(@ModelAttribute @Valid Identity newIdentity, BindingResult bindingResult, Model model)
+	{
+		if(bindingResult.hasErrors())
+			return "editIdentity";
+			identityMapper.update(newIdentity);
+			model.addAttribute("identity", newIdentity);
+			return "redirect:/app/index";
+	}
+	
+	
+	// LIST WITH PAGINATION
+	@RequestMapping(value= {"/index"}, method = RequestMethod.GET)
+	public String indexPage(Model model, @RequestParam("page") Optional<Integer> page, @RequestParam("pageSize") Optional<Integer> size) 
+	{
+		page.ifPresent((p) -> { 
+			if(p>0) 
+				currentPage = p;
+			});
+		size.ifPresent((s) -> { 
+			if(s>0) 
+				pageSize = s;
+			});
+		List<Identity> list = this.identityMapper.findAll((currentPage-1)*pageSize, pageSize);
+		int totalItem = this.identityMapper.countTotalRecords();
+		int NumberPages= totalItem/pageSize + ((totalItem%pageSize == 0) ? 0 : 1);
+		System.out.println(
+				new StringBuilder()
+				.append("*************")
+				.append(System.getProperty("line.separator"))
+				.append("TOTAL RECORDS: ")
+				.append(totalItem)
+				.append(System.getProperty("line.separator"))
+				.append("NUMBER OF PAGE: ")
+				.append(NumberPages)
+				.append(System.getProperty("line.separator"))
+				.append("ITEMS PER PAGE: ")
+				.append(pageSize)
+				.append(System.getProperty("line.separator"))
+				.append("*************")
+				.append(System.getProperty("line.separator"))
+				.append("CURRENT PAGE: ")
+				.append(currentPage));
+		model.addAttribute("identities", list);
+		model.addAttribute("pageSize", pageSize);
+		model.addAttribute("pages", NumberPages);
+		model.addAttribute("currentPage", currentPage);
+		return "index";
+	}
